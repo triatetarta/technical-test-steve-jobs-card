7c194bc384fea69598a20ee673c14c64a5b4d8f3
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-24 11:48:06 +0000

    initial dev env setup

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.scss';
+
+const Card = () => {
+  return (
+    <article className='card'>
+      <h1>hey</h1>
+    </article>
+  );
+};
+
+export default Card;
